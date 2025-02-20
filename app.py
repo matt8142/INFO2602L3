b15fbe029951b5ffd21d402c6755a578897e83dc
matt8142 <157655070+matt8142@users.noreply.github.com>
2025-02-20 03:18:19 +0000

    final commit for completed lab

--- app.py
+++ app.py
@@ -149,10 +149,48 @@ def get_todos_view():
 
 
 # Task 5.3 Here GET /todos/id
+@app.route('/todos/<int:id>', methods=['GET'])
+@jwt_required()
+def get_todo_view(id):
+  todo = Todo.query.get(id)
+
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
+
+  return jsonify(todo.get_json()), 200
+
 
 # Task 5.4 Here PUT /todos/id
+@app.route('/todos/<int:id>', methods=['PUT'])
+@login_required(RegularUser)
+def edit_todo_view(id):
+  data = request.json
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+
+  todo = Todo.query.get(id)
+
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
+
+  user.update_todo(id, data['text'])
+  return jsonify(message=f"todo updated to '{data['text']}'!"), 200
+
 
 # Task 5.5 Here DELETE /todos/id
+@app.route('/todos/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_todo_view(id):
+  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+  todo = Todo.query.get(id)
+
+  # must check if todo belongs to the authenticated user
+  if not todo or todo.user.username != get_jwt_identity():
+    return jsonify(error="Bad ID or unauthorized"), 401
+
+  user.delete_todo(id)
+  return jsonify(message="todo deleted!"), 200
 
 
 @app.route('/todos/stats', methods=['GET'])
